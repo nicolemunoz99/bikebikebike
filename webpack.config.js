b5fb03945c406f7daeb645b74e629c0a6615d014
Nicole Munoz <nicolemunoz@protonmail.com>
2020-03-15 16:51:58 -0600

    initial package, file, compiler setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    path: __dirname + '/client/dist/',
+    filename: 'bundle.js'
+  }
+};
