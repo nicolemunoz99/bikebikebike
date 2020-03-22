b2aa2de9eca9fa796da415551283416223b31734
Nicole Munoz <nicolemunoz@protonmail.com>
2020-03-22 12:52:13 -0600

    concurrent strava and db bike retrieval

--- api/controller/allData.js
+++ api/controller/allData.js
@@ -1,15 +1,19 @@
 const axios = require('axios');
 const stravaApi = require('./stravaApi.js');
+const { get, insert, update } = require('../db.js'); 
 
 const allData = {
   get: async (req, res) => {
     console.log('in allData.get', req.body)
 
-    // Promise.all:
-      // strava: get athlete data/bikes
-      let athleteData = (await stravaApi.get.infoWithBikes(req.body.permissions)).data;
-      console.log('atheleteData', athleteData)
-      // get bikes from db
+    // Promise.all: strava athlete/bikes, db bikes
+    let athleteDataPromise = stravaApi.get.infoWithBikes(req.body.permissions);
+    let bikesInDbPromise = get('bikes', {strava_id: req.body.permissions.id});
+    
+    let [{ ['data']: athleteData }, bikesInDb] = await Promise.all([athleteDataPromise, bikesInDbPromise]);
+
+    console.log('athleteData', athleteData)
+
 
     // Promise.all:
       // for new bikes: stravaApi.get.allUsage
