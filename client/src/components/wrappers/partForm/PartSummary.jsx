932a347a8ccff61b6c7b9c87802a2cf40144de76
Nicole Munoz <nicolemunoz@protonmail.com>
2020-06-04 09:23:39 -0600

    fix summary layout

--- client/src/components/wrappers/partForm/PartSummary.jsx
+++ client/src/components/wrappers/partForm/PartSummary.jsx
@@ -28,8 +28,8 @@ const PartSummary = () => {
       />
         
 
-        <Col xs={{ span: 11, offset: 1 }}>
-          <div className='my-2'>
+        <Col xs={{ span: 8, offset: 4 }}>
+          <div className='my-2 text-left'>
             {lifespan_dist ? <li>{`${lifespan_dist} ${distUnit}`}</li> : ''}
             {lifespan_time ? <li>{`${lifespan_time} hours (ride time)`}</li> : ''}
             {lifespan_date ? <li>{`${xDate(lifespan_date).toString('MMM dd, yyyy')}`}</li> : ''}
